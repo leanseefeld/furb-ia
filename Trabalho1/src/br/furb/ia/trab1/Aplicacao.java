450203ef1809a732509fdc4e687ae530a98911e3
GustavoSabel <gustavo.sabel.gs@gmail.com>
2015-03-26 20:34:48 -0300

    Implementada opção de o usuário escolher o tipo de busca  - Busca em largura  - Busca em profundidade  - Busca iterativa
    E outras coisas mais

--- Trabalho1/src/br/furb/ia/trab1/Aplicacao.java
+++ Trabalho1/src/br/furb/ia/trab1/Aplicacao.java
@@ -2,10 +2,15 @@ package br.furb.ia.trab1;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
-import br.furb.ia.trab1.buscaiterativa.HorarioBuscaIterativa;
+import br.furb.ia.trab1.busca.HorarioBusca;
+import busca.Busca;
 import busca.BuscaIterativo;
+import busca.BuscaLargura;
+import busca.BuscaProfundidade;
 import busca.MostraStatusConsole;
 import busca.Nodo;
 
@@ -35,7 +40,11 @@ public class Aplicacao {
 		Disciplina M = new Disciplina("M", semestre);
 		Disciplina N = new Disciplina("N", semestre);
 		Disciplina O = new Disciplina("O", semestre);
-
+		
+		L.setPreRequisito(H);
+		H.setPreRequisito(B);
+		I.setPreRequisito(A);
+		
 		Disciplina[] priPeriodo = { C, A, B, E, B };
 		Disciplina[] segPeriodo = { D, C, D, A, E };
 		Horario semestre1 = new Horario(new Disciplina[][] { priPeriodo, segPeriodo });
@@ -57,17 +66,47 @@ public class Aplicacao {
 	}
 
 	public static void main(String[] args) {
-		List<Disciplina> disciplinasDisponiveis = new ArrayList<>(DISCIPLINAS_OFERTADAS);
-		disciplinasDisponiveis.removeAll(DISCIPLINAS_CURSADAS);
+		try {
+			List<Disciplina> disciplinasDisponiveis = new ArrayList<>(DISCIPLINAS_OFERTADAS);
+			disciplinasDisponiveis.removeAll(DISCIPLINAS_CURSADAS);
 
-		BuscaIterativo busca = new BuscaIterativo(new MostraStatusConsole());
-		Nodo meta = busca.busca(new HorarioBuscaIterativa(disciplinasDisponiveis));
-		if (meta == null) {
-			System.out.println("Problema sem solução");
-		} else {
-			StringBuilder builder = new StringBuilder();
-			printCaminho(meta, builder);
-			System.out.println(builder);
+			Busca busca = null;
+			String tipoBusca = "";
+
+			System.out.println("Busca em Largura (L), busca com Aprofundamento Iterativa (I) ou Busca em Profundidade (P) ?");
+
+			try (Scanner in = new Scanner(System.in)) {
+				do {
+					MostraStatusConsole console = new MostraStatusConsole();
+					tipoBusca = in.next();
+					if (tipoBusca.equalsIgnoreCase("L")) {
+						busca = new BuscaLargura(console);
+					} else if (tipoBusca.equalsIgnoreCase("P")) {
+						busca = new BuscaProfundidade(console);
+					} else if (tipoBusca.equalsIgnoreCase("I")) {
+						busca = new BuscaIterativo(console);
+						((BuscaIterativo)busca).setProfMax(10);
+					} else {
+						System.out.println("Entrada Inválida!\r\nInforme L ou I");
+					}
+				} while (busca == null);
+			}
+
+			removeDependencias(disciplinasDisponiveis, DISCIPLINAS_CURSADAS);
+			HorarioBusca horarioBusca = new HorarioBusca(disciplinasDisponiveis);
+			Nodo meta = busca.busca(horarioBusca);
+			if (meta == null) {
+				System.out.println("Problema sem solução");
+			} else {
+				StringBuilder builder = new StringBuilder();
+				printCaminho(meta, builder);
+				System.out.println(builder);
+			}
+
+			System.out.print("Fim da busca " + (tipoBusca == "L" ? "em Largura" : (tipoBusca == "I" ? "com Aprofundamento Iterativo": " em Profundidade")));
+
+		} catch (Exception ex) {
+			System.out.println("Erro\r\n" + ex.getMessage());
 		}
 	}
 
@@ -76,6 +115,16 @@ public class Aplicacao {
 			printCaminho(meta.getPai(), builder);
 			builder.append(meta.getEstado().toString()).append(" -----\n");
 		}
+	}
+
+	private static void removeDependencias(List<Disciplina> disciplinasSemDep, List<Disciplina> cursadas) {
+		List<Disciplina> removidos = new LinkedList<>();
+		for (Disciplina disc : disciplinasSemDep) {
+			if (disc.getPreRequisito() != null && !cursadas.contains(disc.getPreRequisito())) {
+				removidos.add(disc);
+			}
+		}
+		disciplinasSemDep.removeAll(removidos);
 	}
 
 }
--- Trabalho1/src/br/furb/ia/trab1/busca/HorarioBusca.java
+++ Trabalho1/src/br/furb/ia/trab1/busca/HorarioBusca.java
@@ -1,4 +1,4 @@
-package br.furb.ia.trab1.buscaiterativa;
+package br.furb.ia.trab1.busca;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,19 +9,18 @@ import br.furb.ia.trab1.Dia;
 import br.furb.ia.trab1.Disciplina;
 import busca.Estado;
 
-public class HorarioBuscaIterativa implements Estado {
+public class HorarioBusca implements Estado {
 
 	private Disciplina[][] horarios = new Disciplina[Dia.values().length][2];
-	// Trocar pra um HashSet quando tiver funcionando pra ver se melhora a performance e comentar no relatório
 	private List<Disciplina> disciplinasDisponiveis = new ArrayList<Disciplina>();
 
-	public HorarioBuscaIterativa(List<Disciplina> disciplinasDisponiveis) {
+	public HorarioBusca(List<Disciplina> disciplinasDisponiveis) {
 		this.disciplinasDisponiveis = new ArrayList<>(disciplinasDisponiveis);
 	}
 
 	@Override
-	public HorarioBuscaIterativa clone() {
-		HorarioBuscaIterativa novaBusca = new HorarioBuscaIterativa(disciplinasDisponiveis);
+	public HorarioBusca clone() {
+		HorarioBusca novaBusca = new HorarioBusca(disciplinasDisponiveis);
 		for (int i = 0; i < horarios.length; i++) {
 			for (int j = 0; j < horarios[i].length; j++) {
 				novaBusca.horarios[i][j] = this.horarios[i][j];
@@ -42,12 +41,7 @@ public class HorarioBuscaIterativa implements Estado {
 
 	@Override
 	public List<Estado> sucessores() {
-		/*
-		 * Pelo que entendi, soh vai terminar quando todas as aulas forem
-		 * preenchidas logo, se para o o horario em questão não tiver disciplina
-		 * disponivel, esse será um caminho sem futuro e ja deve ser descartado
-		 */
-		List<Estado> proximosEstados = new ArrayList<Estado>();
+		List<Estado> proximosEstados = new ArrayList<>();
 
 		lacoDias: for (int iDia = 0; iDia < horarios.length; iDia++) {
 			for (int iPeriodo = 0; iPeriodo < horarios[iDia].length; iPeriodo++) {
@@ -91,7 +85,7 @@ public class HorarioBuscaIterativa implements Estado {
 						 * esse novo estado
 						 */
 						if (disciplinaEncontrada != null) {
-							HorarioBuscaIterativa novaBusca = this.clone();
+							HorarioBusca novaBusca = this.clone();
 							novaBusca.horarios[posicoes[0][0]][posicoes[0][1]] = disciplinaEncontrada;
 							novaBusca.horarios[posicoes[1][0]][posicoes[1][1]] = disciplinaEncontrada;
 							novaBusca.disciplinasDisponiveis.remove(disciplinaEncontrada);
@@ -120,10 +114,10 @@ public class HorarioBuscaIterativa implements Estado {
 	@Override
 	public int hashCode() {
 		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((disciplinasDisponiveis == null) ? 0 : disciplinasDisponiveis.hashCode());
+		/*int result = 1;
 		result = prime * result + Arrays.hashCode(horarios);
-		return result;
+		return result;*/
+		return prime * this.toString().hashCode(); 
 	}
 
 	@Override
@@ -134,14 +128,22 @@ public class HorarioBuscaIterativa implements Estado {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		HorarioBuscaIterativa other = (HorarioBuscaIterativa) obj;
-		if (disciplinasDisponiveis == null) {
-			if (other.disciplinasDisponiveis != null)
-				return false;
-		} else if (disciplinasDisponiveis.size() != other.disciplinasDisponiveis.size() || !disciplinasDisponiveis.containsAll(other.disciplinasDisponiveis))
-			return false;
+		HorarioBusca other = (HorarioBusca) obj;
 		if (!Arrays.deepEquals(horarios, other.horarios))
 			return false;
+		if (!this.ComparaGrade(other.horarios))
+			return false;
+		return true;
+	}
+
+	private boolean ComparaGrade(Disciplina[][] horarios2) {
+		for (int i = 0; i < horarios2.length; i++) {
+			for (int j = 0; j < horarios2[i].length; j++) {
+				if (!horarios2[i][j].equals(this.horarios[i][j])) {
+					return false;
+				}
+			}
+		}
 		return true;
 	}
 
@@ -151,7 +153,7 @@ public class HorarioBuscaIterativa implements Estado {
 		saida.append("SEGUNDA    TERÇA      QUARTA     QUINTA     SEXTA     \n");
 		for (int i = 0; i < horarios[0].length; i++) {
 			for (int j = 0; j < horarios.length; j++) {
-				saida.append(formataString(horarios[j][i], 10));
+				saida.append(formataString(horarios[j][i], 10) + " ");
 			}
 			saida.append('\n');
 		}
