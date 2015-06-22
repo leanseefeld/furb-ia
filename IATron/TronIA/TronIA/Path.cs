0f0933f68b93acfb216775d511ab34f487a44694
Gustavo Sabel <gustavo.sabel.gs@gmail.com>
2015-06-21 22:26:50 -0300

    Inicio da conversão de C# para java

---
+++ IATron/TronIA/TronIA/Path.cs
@@ -0,0 +1,22 @@
+using System;
+
+sealed class Path
+{
+	public string direction;
+    /// <summary>
+    /// Distancia do destino
+    /// </summary>
+	public int length;
+	public float cost;
+
+	public Path(string direction, int length) : this(direction, length, 0)
+	{
+	}
+
+	public Path(string direction, int length, float cost)
+    {
+		this.direction = direction;
+		this.length = length;
+		this.cost = cost;
+	}
+}
